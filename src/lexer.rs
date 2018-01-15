5995ad2560b112a898674d1d02b3413c33080930
Brian Macdonald <brian@brianmacdonald.org>
2018-01-14 17:38:38 -0700

    Added more lexer methods.w

--- src/lexer.rs
+++ src/lexer.rs
@@ -46,6 +46,26 @@ impl Lexer {
         }
     }
 
+    fn peek_char(&mut self) -> char {
+        if self.read_position >= self.input.len() as u16 {
+            return 0 as char;
+        } else {
+            return self.input.chars().nth(self.read_position as usize).unwrap() as char;
+        }
+    }
+
+    fn read_string(&mut self) -> String {
+        let mut position = self.position + 1;
+        let mut done = false;
+        while !done {
+            self.read_char();
+            if self.ch == '"' || self.ch == 0 as char {
+                done = true;
+            }
+        }
+        return self.input.chars().skip(position as usize).take(self.position as usize).collect();
+    }
+
 }
 
 
@@ -81,5 +101,17 @@ mod tests {
         lex.read_char();
         assert_eq!(lex.next_token().t_type, token::ILLEGAL);
     }
+
+    #[test]
+    fn peek_char() {
+        let mut lex = Lexer::new(String::from("fn _call"));
+        assert_eq!(lex.peek_char(), 'n');
+    }
    
+    #[test]
+    fn read_char() {
+        let mut lex = Lexer::new(String::from("\"_call"));
+        assert_eq!(lex.read_string(), String::from("_call"));
+    }
+
 }
